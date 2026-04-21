38b40ae0d7ed8b05043e4c04afef6f98231baeaf
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-21 22:59:48 +0530

    Changes in register Component

--- travel-management/src/app/auth/register/register.component.ts
+++ travel-management/src/app/auth/register/register.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    role: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+}
