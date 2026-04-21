0c3566df07656c20f775c9247c13f237ebbe455b
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-21 23:18:22 +0530

    Make changes in login validations

--- travel-management/src/app/auth/login/login.component.ts
+++ travel-management/src/app/auth/login/login.component.ts
@@ -16,12 +16,25 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  submitted = false;
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(64),
+    ]),
   });
 
   onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
     console.log(this.loginForm.value);
   }
 }
