41eed276d5967524ed7b99572545f47d476f0dba
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-22 11:33:56 +0530

    Make routing working according to role based login

--- travel-management/src/app/app.routes.ts
+++ travel-management/src/app/app.routes.ts
@@ -1,10 +1,26 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './auth/auth.routes';
 
+import { DashboardComponent as EmployeeDashboard } from './dashboards/employee/dashboard/dashboard.component';
+import { DashboardComponent as ManagerDashboard } from './dashboards/manager/dashboard/dashboard.component';
+import { DashboardComponent as FinanceDashboard } from './dashboards/finance/dashboard/dashboard.component';
+
 export const routes: Routes = [
   {
     path: '',
     children: authRoutes,
+  },
+  {
+    path: 'employee',
+    component: EmployeeDashboard,
+  },
+  {
+    path: 'manager',
+    component: ManagerDashboard,
+  },
+  {
+    path: 'finance',
+    component: FinanceDashboard,
   },
   {
     path: '**',
