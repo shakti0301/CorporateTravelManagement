60b118c9490d4b9d11f067882fb52211b7b5589d
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-22 11:14:10 +0530

    Add Register and Login service logic

--- travel-management/src/app/services/auth.service.ts
+++ travel-management/src/app/services/auth.service.ts
@@ -1,9 +1,39 @@
+import { JsonPipe } from '@angular/common';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  register(userData: any) {
+    let users = JSON.parse(localStorage.getItem('users') || '[]');
+    let existingUser = users.find((user: any) => user.email === userData.email);
+
+    if (existingUser) {
+      alert('Email is already registered. Please use a different email.');
+      return;
+    }
+
+    users.push(userData);
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    alert('Registration successful');
+  }
+
+  login(email: string, password: string) {
+    let users = JSON.parse(localStorage.getItem('users') || '[]');
+    let user = users.find(
+      (u: any) => u.email === email && u.password === password,
+    );
+
+    if (user) {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      return user;
+    } else {
+      return null;
+    }
+  }
 }
