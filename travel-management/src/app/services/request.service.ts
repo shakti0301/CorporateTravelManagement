7359ac854f02693bfa8ebde652ffd81029592b78
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-23 21:25:50 +0530

    Request services added

---
+++ travel-management/src/app/services/request.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequestService {
+  createRequest(request: any) {
+    let requests = JSON.parse(localStorage.getItem('requests') || '[]');
+    let user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+
+    if (!user.email) {
+      alert('User not logged in');
+      return;
+    }
+
+    const newRequest = {
+      ...request,
+      userEmail: user.email,
+      managerStatus: 'pending',
+      financeStatus: 'pending',
+      finalStatus: 'pending',
+      id: Date.now(),
+    };
+
+    requests.push(newRequest);
+    localStorage.setItem('requests', JSON.stringify(requests));
+  }
+
+  getRequestsByUser() {
+    const request = JSON.parse(localStorage.getItem('requests') || '[]');
+    const user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+
+    return request.filter((req: any) => req.userEmail === user.email);
+  }
+
+  getAllRequests() {
+    return JSON.parse(localStorage.getItem('requests') || '[]');
+  }
+
+  updateRequestStatus(id: number, status: string) {
+    let requests = JSON.parse(localStorage.getItem('requests') || '[]');
+
+    requests = requests.map((req: any) => {
+      if (req.id === id) {
+        req.managerStatus = status;
+
+        if (status === 'rejected') {
+          req.financeStatus = 'rejected';
+        }
+      }
+      return req;
+    });
+    localStorage.setItem('requests', JSON.stringify(requests));
+  }
+
+  updateFinanceStatus(id: number, status: string) {
+    let requests = JSON.parse(localStorage.getItem('requests') || '[]');
+
+    requests = requests.map((req: any) => {
+      if (req.id === id) {
+        req.financeStatus = status;
+
+        if (status === 'rejected') {
+          req.finalStatus = 'rejected';
+        }
+
+        if (req.managerStatus === 'approved' && status === 'approved') {
+          req.finalStatus = 'approved';
+        }
+      }
+      return req;
+    });
+  }
+}
