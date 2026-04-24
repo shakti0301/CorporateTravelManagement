e88ddb9742cef201394c37ec0e08c759aec0fa0e
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-24 11:34:54 +0530

    use request services

--- travel-management/src/app/portal/finance/dashboard/dashboard.component.ts
+++ travel-management/src/app/portal/finance/dashboard/dashboard.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../../shared/navbar/navbar.component';
+import { RequestService } from '../../../services/request.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  requests: any[] = [];
+  constructor(private requestService: RequestService) {}
+
+  ngOnInit() {
+    this.loadRequests();
+  }
+
+  loadRequests() {
+    const allRequests = this.requestService.getAllRequests();
+
+    this.requests = allRequests.filter(
+      (req: any) =>
+        req.managerStatus === 'approved' && req.financeStatus === 'pending',
+    );
+  }
+
+  approve(id: number) {
+    this.requestService.updateFinanceStatus(id, 'approved');
+    this.loadRequests();
+  }
+  reject(id: number) {
+    this.requestService.updateFinanceStatus(id, 'rejected');
+    this.loadRequests();
+  }
+}
