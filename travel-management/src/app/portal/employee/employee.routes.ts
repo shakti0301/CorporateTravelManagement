c42b2896fe500a994334990475fc6279fe64b3e9
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-25 20:58:06 +0530

    Set route for expense form

--- travel-management/src/app/portal/employee/employee.routes.ts
+++ travel-management/src/app/portal/employee/employee.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
+import { ExpenseComponent } from './expense/expense.component';
 
 export const employeeRoutes: Routes = [
   {
@@ -10,5 +11,9 @@ export const employeeRoutes: Routes = [
   {
     path: 'request',
     component: TravelRequestComponent,
+  },
+  {
+    path: 'expense/:id',
+    component: ExpenseComponent,
   },
 ];
