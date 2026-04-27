81037c6fa17a7eb06bd14b3e9d57f09202c67f6b
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-27 11:43:36 +0530

    Add component my-request and set route of it

--- travel-management/src/app/portal/employee/employee.routes.ts
+++ travel-management/src/app/portal/employee/employee.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
 import { ExpenseComponent } from './expense/expense.component';
+import { MyRequestsComponent } from './my-requests/my-requests.component';
 
 export const employeeRoutes: Routes = [
   {
@@ -15,5 +16,9 @@ export const employeeRoutes: Routes = [
   {
     path: 'expense/:id',
     component: ExpenseComponent,
+  },
+  {
+    path: 'myrequests',
+    component: MyRequestsComponent,
   },
 ];
