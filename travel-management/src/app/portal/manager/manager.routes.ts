bdcc62de06bf0c80511717eda72b993aa2c2476b
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-27 11:49:56 +0530

    Set route for approvals

--- travel-management/src/app/portal/manager/manager.routes.ts
+++ travel-management/src/app/portal/manager/manager.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ApprovalsComponent } from './approvals/approvals.component';
 
 export const managerRoutes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+  },
+  {
+    path: 'approval',
+    component: ApprovalsComponent,
   },
 ];
