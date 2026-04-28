7fb71041d9da0d0a9102679b34b9e0d5cd6e09b7
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-28 12:50:38 +0530

    Create Component for Edit Draft

---
+++ travel-management/src/app/portal/employee/edit-request/edit-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-request.component.html',
+  styleUrl: './edit-request.component.css'
+})
+export class EditRequestComponent {
+
+}
