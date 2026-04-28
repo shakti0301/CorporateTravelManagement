2824f7266d3226879836f3db37d3cf3c925d5745
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-28 14:18:13 +0530

    Route for Draft's Form

--- travel-management/src/app/portal/employee/employee.routes.ts
+++ travel-management/src/app/portal/employee/employee.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
 import { ExpenseComponent } from './expense/expense.component';
 import { MyRequestsComponent } from './my-requests/my-requests.component';
+import { EditRequestComponent } from './edit-request/edit-request.component';
 
 export const employeeRoutes: Routes = [
   {
@@ -20,5 +21,9 @@ export const employeeRoutes: Routes = [
   {
     path: 'myrequests',
     component: MyRequestsComponent,
+  },
+  {
+    path: 'edit/:id',
+    component: EditRequestComponent,
   },
 ];
