f4b854b43ff8f889ea8f7be9de752de1e5fd2730
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-04-30 11:42:56 +0530

    implement services in to .ts

--- travel-management/src/app/portal/finance/reimbursements/reimbursements.component.ts
+++ travel-management/src/app/portal/finance/reimbursements/reimbursements.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { ReimbursementService } from '../../../services/reimbursement/reimbursement.service';
 
 @Component({
   selector: 'app-reimbursements',
@@ -14,13 +15,10 @@ export class ReimbursementsComponent {
   selectedRequest: any = null;
   showModal: boolean = false;
 
-  ngOnInit() {
-    const requests = JSON.parse(localStorage.getItem('requests') || '[]');
+  constructor(private reimbursementService: ReimbursementService) {}
 
-    this.reimbursementRequests = requests.filter(
-      (req: any) =>
-        req.expenseSubmitted === true && req.reimbursementStatus === 'pending',
-    );
+  ngOnInit() {
+    this.refreshList();
   }
 
   openDetails(req: any) {
@@ -49,55 +47,24 @@ export class ReimbursementsComponent {
   }
 
   approveRequest() {
-    let requests = JSON.parse(localStorage.getItem('requests') || '[]');
-
-    requests = requests.map((r: any) => {
-      if (r.id === this.selectedRequest.id) {
-        return {
-          ...r,
-          reimbursementStatus: 'approved',
-          reimbursementRemark: 'Approved by finance',
-        };
-      }
-      return r;
-    });
-
-    localStorage.setItem('requests', JSON.stringify(requests));
+    this.reimbursementService.approve(this.selectedRequest.id);
 
     this.closeModal();
     this.refreshList();
   }
 
   rejectRequest() {
-    let reason = prompt('Enter rejection reason');
+    const reason = prompt('Enter rejection reason');
 
     if (!reason) return;
 
-    let requests = JSON.parse(localStorage.getItem('requests') || '[]');
-
-    requests = requests.map((r: any) => {
-      if (r.id === this.selectedRequest.id) {
-        return {
-          ...r,
-          reimbursementStatus: 'rejected',
-          reimbursementRemark: reason,
-        };
-      }
-      return r;
-    });
-
-    localStorage.setItem('requests', JSON.stringify(requests));
+    this.reimbursementService.reject(this.selectedRequest.id, reason);
 
     this.closeModal();
     this.refreshList();
   }
 
   refreshList() {
-    const requests = JSON.parse(localStorage.getItem('requests') || '[]');
-
-    this.reimbursementRequests = requests.filter(
-      (req: any) =>
-        req.expenseSubmitted === true && req.reimbursementStatus === 'pending',
-    );
+    this.reimbursementRequests = this.reimbursementService.getPendingRequests();
   }
 }
