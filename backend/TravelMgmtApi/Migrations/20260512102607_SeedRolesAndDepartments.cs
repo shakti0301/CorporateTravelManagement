1d0a545489061868b3e8c3e30e9fac7190742004
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-12 16:41:44 +0530

    Run migration for data (Roles and Department)

---
+++ backend/TravelMgmtApi/Migrations/20260512102607_SeedRolesAndDepartments.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TravelMgmtApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRolesAndDepartments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName", "ManagerId" },
+                values: new object[,]
+                {
+                    { 1, ".NET", null },
+                    { 2, "Java", null },
+                    { 3, "QA", null },
+                    { 4, "AI", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Employee" },
+                    { 3, "Manager" },
+                    { 4, "Finance" },
+                    { 5, "ProjectManager" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "DepartmentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "DepartmentId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "DepartmentId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Departments",
+                keyColumn: "DepartmentId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 5);
+        }
+    }
+}
