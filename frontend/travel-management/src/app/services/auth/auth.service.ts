f84d250454ac4f8daf517414c176025fe71301c9
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-13 11:37:29 +0530

    Set Service according to Backend

--- frontend/travel-management/src/app/services/auth/auth.service.ts
+++ frontend/travel-management/src/app/services/auth/auth.service.ts
@@ -1,56 +1,60 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  register(userData: any) {
-    let users = JSON.parse(localStorage.getItem('users') || '[]');
-    let existingUser = users.find((user: any) => user.email === userData.email);
+  private apiUrl = `${environment.apiUrl}/Auth`;
 
-    if (existingUser) {
-      alert('Email is already registered. Please use a different email.');
-      return;
-    }
+  constructor(private http: HttpClient) {}
 
-    users.push(userData);
-    localStorage.setItem('users', JSON.stringify(users));
+  // Register API
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
   }
 
-  login(email: string, password: string) {
-    let users = JSON.parse(localStorage.getItem('users') || '[]');
-    let user = users.find(
-      (u: any) => u.email === email && u.password === password,
-    );
-
-    if (user) {
-      localStorage.setItem('currentUser', JSON.stringify(user));
-      return user;
-    } else {
-      return null;
-    }
+  // Login API
+  login(loginData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, loginData);
   }
+
+  // Redirect Based On Role
 
   getRedirectPath(role: string): string {
     switch (role.toLowerCase()) {
       case 'employee':
         return '/employee';
+
       case 'projectmanager':
         return '/pm';
+
       case 'manager':
         return '/manager';
+
       case 'finance':
         return '/finance';
+
+      case 'admin':
+        return '/admin';
+
       default:
         return '/';
     }
   }
 
+  // Current User
   getCurrentUser() {
     return JSON.parse(localStorage.getItem('currentUser') || 'null');
   }
 
+  // Logout
   logout() {
+    localStorage.removeItem('token');
     localStorage.removeItem('currentUser');
+    localStorage.removeItem('role');
+    localStorage.removeItem('userName');
   }
 }
