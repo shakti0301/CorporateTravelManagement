460fcf23432abd245d4f3eaa1033bc8c55a3b604
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-14 22:31:12 +0530

    Create 2 enums for approvals

---
+++ backend/TravelMgmtApi/Enums/ApprovalStage.cs
@@ -0,0 +1,10 @@
+namespace TravelMgmtApi.Enums
+{
+    public enum ApprovalStage
+    {
+        ProjectManager = 1,
+        Manager = 2,
+        Finance = 3,
+        Completed = 4,
+    }
+}
---
+++ backend/TravelMgmtApi/Enums/RequestStatus.cs
@@ -0,0 +1,9 @@
+namespace TravelMgmtApi.Enums
+{
+    public enum RequestStatus
+    {
+        Pending = 1,
+        Approved = 2,
+        Rejected = 3,
+    }
+}
