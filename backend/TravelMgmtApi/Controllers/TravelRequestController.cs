81cb454a65a70ac9a7ff189067c6dc1d6cecec05
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-15 14:49:09 +0530

    Create new controller for request

---
+++ backend/TravelMgmtApi/Controllers/TravelRequestController.cs
@@ -0,0 +1,58 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TravelMgmtApi.DTOs;
+using TravelMgmtApi.Interfaces;
+
+namespace TravelMgmtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TravelRequestController : ControllerBase
+    {
+        private readonly ITravelRequestService _travelService;
+
+        public TravelRequestController(ITravelRequestService travelService)
+        {
+            _travelService = travelService;
+        }
+
+        //Create Travel Request
+        [HttpPost]
+        public async Task<IActionResult> CreateRequest(CreateTravelRequestDto dto)
+        {
+            //Get user id from token
+            var userId = User.FindFirst(
+                ClaimTypes.NameIdentifier
+                )?.Value;
+            if (userId == null)           
+            {
+                return Unauthorized();
+            }
+
+            var result = await _travelService.CreateRequestAsync(Convert.ToInt32(userId), dto);
+            return Ok(new
+            {
+                message = result
+            });
+        }
+
+        //My Requests
+        [HttpGet("my")]
+        public async Task<IActionResult> GetMyRequests()
+        {
+            //Get user id from token
+            var userId = User.FindFirst(
+                ClaimTypes.NameIdentifier
+                )?.Value;
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _travelService.GetMyRequestsAsync(Convert.ToInt32(userId));
+            return Ok(result);
+        }
+    }
+}
