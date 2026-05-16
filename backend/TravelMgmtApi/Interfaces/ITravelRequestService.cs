3057703bdf9626fbe40fff515cfb91fa66d739ad
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-16 18:25:48 +0530

    Add interface for finance

--- backend/TravelMgmtApi/Interfaces/ITravelRequestService.cs
+++ backend/TravelMgmtApi/Interfaces/ITravelRequestService.cs
@@ -7,7 +7,10 @@ namespace TravelMgmtApi.Interfaces
     {
         Task<string> CreateRequestAsync(int employeeId, CreateTravelRequestDto dto);
         Task<List<TravelRequestResponseDto>> GetMyRequestsAsync(int employeeId);
+
         Task<List<TravelRequestResponseDto>>GetPendingManagerRequestsAsync(int managerId);
         Task<string> ApproveOrRejectAsync(int approverId, ApprovalActionDto dto);
+
+        Task<List<TravelRequestResponseDto>>GetPendingFinanceRequestsAsync(int financeId);
     }
 }
