24ec94532508ec3ae82970b1f00bc18c805065a4
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-18 13:01:01 +0530

    Create DTO for User (Dropdown - PM)

---
+++ backend/TravelMgmtApi/DTOs/UserDropdownDto.cs
@@ -0,0 +1,8 @@
+namespace TravelMgmtApi.DTOs
+{
+    public class UserDropdownDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+    }
+}
