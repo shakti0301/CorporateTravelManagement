3315b388a5f49c4df97b7b68ac01b0a13ab9af30
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-18 13:09:19 +0530

    Create new service for Users

---
+++ frontend/travel-management/src/app/services/user/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = `${environment.apiUrl}/User`;
+
+  constructor(private http: HttpClient) {}
+
+  getProjectManagers() {
+    return this.http.get(`${this.apiUrl}/projectmanagers`);
+  }
+}
