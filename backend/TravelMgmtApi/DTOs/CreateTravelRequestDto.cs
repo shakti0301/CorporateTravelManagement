3dde3a95470e99bb5e798524416e6c082aff58ba
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-18 15:13:38 +0530

    Make changes because of draft functionality

--- backend/TravelMgmtApi/DTOs/CreateTravelRequestDto.cs
+++ backend/TravelMgmtApi/DTOs/CreateTravelRequestDto.cs
@@ -2,23 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TravelMgmtApi.DTOs
 {
-    public class CreateTravelRequestDto
+   public class CreateTravelRequestDto
     {
-        //Optional PM
         public int? ProjectManagerId { get; set; }
 
-        [Required]
-        [MaxLength(200)]
-        public string Source { get; set; } = string.Empty;
-        [Required]
-        [MaxLength(200)]
-        public string Destination { get; set; } = string.Empty;
-        [Required]
-        [MaxLength(500)]
-        public string Purpose { get; set; } = string.Empty;
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public decimal EstimatedCost { get; set; }
+        public string? Source { get; set; }
+
+        public string? Destination { get; set; }
+
+        public string? Purpose { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public decimal? EstimatedCost { get; set; }
+
         public bool IsDraft { get; set; }
     }
 }
