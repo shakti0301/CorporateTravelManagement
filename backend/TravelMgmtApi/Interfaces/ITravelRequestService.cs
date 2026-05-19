82443c120e1b13414fdeb70fb392f4869803acf1
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-19 17:43:41 +0530

    Create 2 interface

--- backend/TravelMgmtApi/Interfaces/ITravelRequestService.cs
+++ backend/TravelMgmtApi/Interfaces/ITravelRequestService.cs
@@ -8,6 +8,8 @@ namespace TravelMgmtApi.Interfaces
         Task<string> CreateRequestAsync(int employeeId, CreateTravelRequestDto dto);
         Task<List<TravelRequestResponseDto>> GetMyRequestsAsync(int employeeId);
         Task<List<TravelRequestResponseDto>> GetDraftRequestsAsync(int employeeId);
+        Task<List<TravelRequestResponseDto>> GetManagerRequestsAsync(int managerId);
+        Task<List<TravelRequestResponseDto>>GetPMRequestsAsync(int pmId);
         Task<List<TravelRequestResponseDto>>GetPendingPMRequestsAsync(int projectManagerId);   
         Task<List<TravelRequestResponseDto>>GetPendingManagerRequestsAsync(int managerId);
         Task<List<TravelRequestResponseDto>>GetPendingFinanceRequestsAsync(int financeId);
