157d16f95787cd12e58c94e1707b8d4648f0de51
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-19 11:00:04 +0530

    Change routes

--- frontend/travel-management/src/app/components/portal/manager/manager.routes.ts
+++ frontend/travel-management/src/app/components/portal/manager/manager.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ApprovalsComponent } from './approvals/approvals.component';
+import { TravelRequestComponent } from './travel-request/travel-request.component';
+import { MyRequestsComponent } from './my-requests/my-requests.component';
+import { RequestDetailsComponent } from './request-details/request-details.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { ItineraryComponent } from './itinerary/itinerary.component';
 
 export const managerRoutes: Routes = [
   {
@@ -10,5 +15,25 @@ export const managerRoutes: Routes = [
   {
     path: 'approval',
     component: ApprovalsComponent,
+  },
+  {
+    path: 'request',
+    component: TravelRequestComponent,
+  },
+  {
+    path: 'myrequests',
+    component: MyRequestsComponent,
+  },
+  {
+    path: 'request-details/:id',
+    component: RequestDetailsComponent,
+  },
+  {
+    path: 'expense/:id',
+    component: ExpenseComponent,
+  },
+  {
+    path: 'itinerary/:id',
+    component: ItineraryComponent,
   },
 ];
