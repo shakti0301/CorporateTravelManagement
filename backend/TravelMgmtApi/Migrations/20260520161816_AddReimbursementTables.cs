fb96d5275d28f5070febd97daeeb0832800ae77f
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-20 22:04:00 +0530

    Run migration for totalexpense

---
+++ backend/TravelMgmtApi/Migrations/20260520161816_AddReimbursementTables.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelMgmtApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReimbursementTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalExpense",
+                table: "Reimbursements",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalExpense",
+                table: "Reimbursements");
+        }
+    }
+}
