49ec072592a3200978919319ac6ff965301a0d40
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-20 23:15:49 +0530

    run migration after add relation in DBcontext

---
+++ backend/TravelMgmtApi/Migrations/20260520174225_AddReimbursementRelationships.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelMgmtApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReimbursementRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reimbursements_TravelRequestId",
+                table: "Reimbursements");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TravelRequestId1",
+                table: "Reimbursements",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reimbursements_TravelRequestId",
+                table: "Reimbursements",
+                column: "TravelRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reimbursements_TravelRequestId1",
+                table: "Reimbursements",
+                column: "TravelRequestId1",
+                unique: true,
+                filter: "[TravelRequestId1] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reimbursements_TravelRequests_TravelRequestId1",
+                table: "Reimbursements",
+                column: "TravelRequestId1",
+                principalTable: "TravelRequests",
+                principalColumn: "TravelRequestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reimbursements_TravelRequests_TravelRequestId1",
+                table: "Reimbursements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reimbursements_TravelRequestId",
+                table: "Reimbursements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reimbursements_TravelRequestId1",
+                table: "Reimbursements");
+
+            migrationBuilder.DropColumn(
+                name: "TravelRequestId1",
+                table: "Reimbursements");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reimbursements_TravelRequestId",
+                table: "Reimbursements",
+                column: "TravelRequestId",
+                unique: true);
+        }
+    }
+}
