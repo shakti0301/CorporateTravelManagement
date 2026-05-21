bdccb76b6c0f912a6dcafdfe5c166eadc9d6c394
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-21 12:11:28 +0530

    Run migration

---
+++ backend/TravelMgmtApi/Migrations/20260521062352_UpdateReimbursementFlow.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelMgmtApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReimbursementFlow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reimbursements_TravelRequests_TravelRequestId",
+                table: "Reimbursements");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reimbursements_TravelRequests_TravelRequestId",
+                table: "Reimbursements",
+                column: "TravelRequestId",
+                principalTable: "TravelRequests",
+                principalColumn: "TravelRequestId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reimbursements_TravelRequests_TravelRequestId",
+                table: "Reimbursements");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reimbursements_TravelRequests_TravelRequestId",
+                table: "Reimbursements",
+                column: "TravelRequestId",
+                principalTable: "TravelRequests",
+                principalColumn: "TravelRequestId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
