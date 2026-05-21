17acac93d76d7d08103a604a16002315c31c6f79
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-21 12:07:47 +0530

    Add one method for add expense

--- backend/TravelMgmtApi/Repositories/Interfaces/IReimbursementRepository.cs
+++ backend/TravelMgmtApi/Repositories/Interfaces/IReimbursementRepository.cs
@@ -6,6 +6,7 @@ namespace TravelMgmtApi.Interfaces
     public interface IReimbursementRepository
     {
         Task<TravelRequest?> GetTravelRequestAsync(int travelRequestId);
+        Task AddExpenseAsync(Expense expense);
         Task AddReimbursementAsync(Reimbursement reimbursement);
         Task<List<ReimbursementResponseDto>> GetFinanceRequestsAsync();
         Task<List<ReimbursementResponseDto>> GetMyReimbursementsAsync(int employeeId);
