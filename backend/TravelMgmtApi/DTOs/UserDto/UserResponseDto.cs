e85c6c75f11bf89b71f0384b3fa4d11029d31266
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-27 21:29:27 +0530

    changes

--- backend/TravelMgmtApi/DTOs/UserDto/UserResponseDto.cs
+++ backend/TravelMgmtApi/DTOs/UserDto/UserResponseDto.cs
@@ -6,7 +6,9 @@ namespace TravelMgmtApi.DTOs
         public string UserName { get; set; } = "";
         public string Email { get; set; } = "";
         public string Role { get; set; } = "";
-        public string? Department { get; set; }
-        public string? Status { get; set; }
+        public int RoleId { get; set; }
+        public string Department { get; set; } = "";
+        public int? DepartmentId { get; set; }
+        public string Status { get; set; } = "";
     }
 }
