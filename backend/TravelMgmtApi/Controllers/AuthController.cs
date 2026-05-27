c47953dd897cb16847dc252731d78e50bcb53f8a
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-27 22:21:57 +0530

    Create endpoints for policy - get, update

--- backend/TravelMgmtApi/Controllers/AuthController.cs
+++ backend/TravelMgmtApi/Controllers/AuthController.cs
@@ -97,5 +97,38 @@ namespace TravelMgmtApi.Controllers
             var result = await _authService.AdminUpdateUserAsync(id,dto);
             return Ok(new {message=result});
         }
+
+        [Authorize(Roles ="Admin")]
+        [HttpPost("policy")]
+        public async Task<IActionResult>
+        AddPolicy(CreatePolicyDto dto)
+        {
+            var result = await _authService.AddPolicyAsync(dto);
+
+            return Ok(
+                new { message=result }
+            );
+        }
+
+        [Authorize(Roles="Admin")]
+        [HttpGet("policy")]
+        public async Task<IActionResult>GetPolicies()
+        {
+            return Ok(
+                await _authService
+                .GetAllPoliciesAsync()
+            );
+        }
+
+        [Authorize(Roles="Admin")]
+        [HttpPut("policy/{id}")]
+        public async Task<IActionResult>UpdatePolicy(int id, UpdatePolicyDto dto)
+        {
+            var result =await _authService.UpdatePolicyAsync(id, dto);
+
+            return Ok(
+                new {message=result}
+            );
+        }
     }
 }
