6e55fcab1f85e7534e738f122809ed840251cb26
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-05-28 17:31:24 +0530

    Add Select stetements

--- Database/TravelManagement.sql
+++ Database/TravelManagement.sql
@@ -11,3 +11,6 @@ SELECT * FROM TravelRequests
 SELECT * FROM TravelRequestApprovals
 SELECT * FROM Reimbursements
 SELECT * FROM Expenses
+SELECT * FROM Activities
+SELECT * FROM ItineraryDays
+SELECT * FROM TravelPolicies
